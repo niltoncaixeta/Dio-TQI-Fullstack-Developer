8b442dc3afe2570aa6f0c810c351e4c8b1923b8e
niltoncaixeta <niltoncaixeta@gmail.com>
2022-06-01 14:44:34 -0300

    Sintaxe básica em JavaScript

---
+++ Modulo III/3-Sintaxe Básica em JavaScript/Aula 2 - Entendendo variáveis e seus valores/script.js
@@ -0,0 +1,137 @@
+// ----------------------------------------
+//  tipos primitivos:
+//  boolean, null, undefined, number, string, array, object, function.
+//  ---------------------------------------
+console.log('Tipos primitivos:');
+console.log('boolean, null, undefined, number, string, array, object, function.');
+
+
+/*
+// tipo boolean
+var boolean = false;
+console.log('A variável ${boolean} tem o tipo ${typeof(boolean)}');
+
+// number
+var numero = 1;
+console.log(`A variável ${numero} tem o tipo ${typeof(numero)}`);
+
+// string
+var nome = 'Nilton';
+console.log(`A variável ${nome} tem o tipo ${typeof(nome)}`);
+
+// function
+var funcao = function(){};
+console.log(`A variável ${funcao} tem o tipo ${typeof(funcao)}`);
+
+// declarando variáveis
+// var   - variável global ou local cujo valor inicial é nulo
+// let   - variavel local de bloco cujo valor inicial é nulo
+// const - variável local de bloco cujo valor inicial é obrigatório, sendo necessário defini-lo na sua criação
+
+
+// utilizando var global
+var nome = 'Nilton';
+
+// utilizando var local
+function nome_sobrenome() {
+    var sobrenome = 'Caixeta';
+    console.log(sobrenome);
+}
+console.log(nome);
+nome_sobrenome();
+
+// as propriedades de var se aplicam a let,
+// mas, por convenção, usamos let no escopo local.
+
+/*
+
+// usando const
+const constante = 'constante';
+constante = 'Novo Valor' 
+//== ocorrerá erro 
+// pois o valor não pode ser alterado;
+
+*/
+
+
+
+/*
+// atribuição
+
+var atribuicao = 'diana';
+
+// comparação
+var comparacao = "0" == 0;
+console.log(comparacao);
+
+// comparação estrita
+var comparacaoestrita = "0" === 0;
+console.log(comparacaoestrita);
+*/
+
+
+/*
+// operadores aritméticos
+
+// adição
+var adicao = 1 + 1;
+console.log(adicao);
+
+//subtração
+var subtracao = 3 - 1;
+console.log(subtracao);
+
+//multiplicação
+var multiplicacao = 4 * 2;
+console.log(multiplicacao);
+
+//divisão real
+var divisaoReal = 6 / 2;
+console.log(divisaoReal);
+
+//divisão inteira
+var modulo = 5 % 2; //divisao inteira
+console.log(modulo);
+
+//potenciação
+var potencia = 2**10;
+console.log(potencia);
+*/
+
+
+/*
+// operadores relacionais
+
+// maior que
+var maiorQue = 3 > 2;
+console.log(maiorQue);
+
+// menor que
+var menorQue = 3 < 2;
+console.log(menorQue);
+
+// maior ou igual a
+var maiorOuIgual = 3 >= 2;
+console.log(maiorOuIgual);
+
+// menor ou igual a
+var menorOuIgual = 3 <= 2;
+console.log(menorOuIgual); */
+
+
+/*
+// operadores lógicos
+
+// && - (And) todos os valores devem ser true
+// || - (Or)  apenas um valor deve ser true
+// !  - (Not) inverte os valores
+
+var e = true && true;
+console.log(e);
+
+var ou = true || false;
+console.log(ou);
+
+var nao = !true;
+console.log(nao);
+*/
--- Modulo III/Introdução ao JavaScript/README.md
+++
@@ -1,5 +0,0 @@
-# Introdução ao javascript
-
-- Contador (Incluído range de valores e botão para zerar o contador)
-- To-do list (Incluído botão para remover tarefa)
-
