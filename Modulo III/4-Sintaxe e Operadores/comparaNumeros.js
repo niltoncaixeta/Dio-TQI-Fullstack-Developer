950141c110894c5f5afc78f010ccbc36bb551051
niltoncaixeta <niltoncaixeta@gmail.com>
2022-06-03 09:59:52 -0300

    Sintaxe e Operadores

--- Modulo III/4-Sintaxe e Operadores/comparaNumeros.js
+++ Modulo III/4-Sintaxe e Operadores/comparaNumeros.js
@@ -3,27 +3,28 @@ const resultado = document.getElementById('resultado');
 
 function comparaNumeros(vlr1, vlr2) {
     let soma = vlr1 + vlr2
-    let result = (vlr1 == vlr2) ? '' : ' não';
-    result = 'Os números ' + vlr1 + ' e ' + vlr2 + result + ' são iguais.';
-    result += ' Sua soma é ' + soma + ', que é ';
-    if (soma == 10) {
-        result += 'igual';
-    } else if (soma < 10) {
-        result += 'menor';
-    } else {
-        result += 'maior';
-    }
+
+    let result = (vlr1 === vlr2) ? '' : 'não ';
+
+    result = `Os números ${vlr1} e ${vlr2} ${result}são iguais.`;
+
+    result += ` Sua soma é ${soma}, que é `;
+
+    result += (soma===10) ? "igual" : (soma>10) ? "maior" : "menor"
+
     result += ' que 10 e ';
-    if (soma == 20) {
-        result += 'igual';
-    } else if (soma < 20) {
-        result += 'menor';
-    } else {
-        result += 'maior';
-    }
+
+    result += (soma===20) ? "igual" : (soma>20) ? "maior" : "menor"
+
     result += ' que 20.';
+
     //console.log(result);
     return result;
+}
+
+function isNumber(varToCheck) {
+    let xreturn = !Number.isNaN(+varToCheck)
+    return xreturn
 }
 
 form.onsubmit = function (e) {
@@ -31,15 +32,18 @@ form.onsubmit = function (e) {
     let valor1 = document.getElementById('numero1');
     valor1.focus();
 	valor1 = Number(valor1.value);
+
 	const valor2 = Number(document.getElementById('numero2').value);
-    if (!valor1) {
+    
+    if (!isNumber(valor1)) {
         resultado.innerHTML='Valor 1 não é um número ou é inválido!';
         return;
     }
-    if (!valor2) {
+    if (!isNumber(valor2)) {
         resultado.innerHTML='Valor 2 não é um número ou é inválido!';
         return;
     }
+
     resultado.innerHTML= comparaNumeros(valor1, valor2);
     form.reset();
 };
